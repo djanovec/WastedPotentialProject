fefdb8723584b5807a368fc08f42ab7a65198fdd
Ian Gurney <ianclaytongurney@gmail.com>
2019-06-04 10:19:33 -0500

    express setup

---
+++ quizApp/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const routes = require('./routes/index');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+module.expores = app;

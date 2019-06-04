ac65edf75012f5b127ab3e8bcbf28b801d211ee2
andrewgray0702 <andrew.gray@midlandu.edu>
2019-06-04 14:22:17 -0500

    addiitions

--- quizApp/models/quiz.models.js
+++ quizApp/models/quiz.models.js
@@ -6,7 +6,6 @@ async function getQuiz(req, res){
     try{
         const getRes = await Quizzes.find();
         console.log(getRes);
-        res.send(getRes);
     }catch(err){
         console.log('Error:' + '' + err);
     }
@@ -19,7 +18,7 @@ function postQuiz(req, res){
             instructions: 'take the damn quiz',
             questions: ['this is a question', 'also a question', 'oh look another question'],
         })
-        Quizzes.create({__id: 2}, function(err, postReq){
+        Quizzes.create({postReq}, function(err, postReq){
             if(err){
                 return console.log('Error:' + ' ' + err)
             }

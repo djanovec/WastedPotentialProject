5dc23c4384fbf6ec16fb12aa6eca351d5718b7b4
andrewgray0702 <andrew.gray@midlandu.edu>
2019-06-04 14:11:30 -0500

    get/post requests working

--- quizApp/models/quiz.models.js
+++ quizApp/models/quiz.models.js
@@ -1,25 +1,27 @@
 const mongoose = require('mongoose');
 const Quizzes = require('../schema/Quizzes');
 
+
 async function getQuiz(req, res){
     try{
         const getRes = await Quizzes.find();
+        console.log(getRes);
+        res.send(getRes);
     }catch(err){
         console.log('Error:' + '' + err);
     }
 }
 function postQuiz(req, res){
         const postReq = new Quizzes({
-            __id: 1,
             hash: 'abc123',
             title: 'Super Cool Quiz',
             description: 'A quiz thats cool',
             instructions: 'take the damn quiz',
             questions: ['this is a question', 'also a question', 'oh look another question'],
         })
-        postReq.save(function (err, post){
+        Quizzes.create({__id: 2}, function(err, postReq){
             if(err){
-                console.log('Error:' + ' ' + err);
+                return console.log('Error:' + ' ' + err)
             }
         })
 }

d7a4c767f7cb135429f1aa6be2aad237e8afd7a7
Grayton Savickas <graytonsavickas@Mandalorian.local>
2019-06-04 14:16:38 -0500

    first commit

---
+++ quizApp/models/quizValidation.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Connect to the db
+mongoose.connect("mongodb://", function (err, db) {
+   
+     if(err) throw err;
+
+     //Write databse Insert/Update/Query code here..
+                
+});

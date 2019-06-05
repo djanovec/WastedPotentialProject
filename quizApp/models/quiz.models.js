346382d66d034b2faa2f6f0a5bcba6c98d679518
Grayton Savickas <graytonsavickas@Mandalorian.local>
2019-06-05 16:50:48 -0500

    added postAnswers and getResults models with routes

--- quizApp/models/quiz.models.js
+++ quizApp/models/quiz.models.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 const Quizzes = require('../schema/Quizzes');
+const Answers = require('../schema/Answers');
 
 
-async function getQuiz(req, res){
-    try{
-        const getRes = await Quizzes.find();
+
+async function getQuiz(req, res) {
+    try {
+        const getRes = await Quizzes.findOne();
         console.log(getRes);
         res.status(201).send(getRes);
-    }catch(err){
+    } catch (err) {
         console.log('Error:' + '' + err);
     }
 }
-async function postQuiz(req, res){
-    try{
+async function postQuiz(req, res) {
+    try {
         const postReq = new Quizzes({
             hash: 'abc123',
             title: 'Super Cool Quiz',
@@ -23,39 +25,83 @@ async function postQuiz(req, res){
         var postRes = await postReq.save();
         console.log(postRes);
         res.status(201).send('Added quiz')
-    } catch(err){
+    } catch (err) {
         console.log('Error:' + ' ' + err);
     }
-
 }
 
-async function getQuizResults(req, res){
-    try{
-        const getRes = await Quizzes.find();
+async function postAnswers(req, res) {
+    try {
+        var quizId
+        const postAnswers = new Answers({
+            _id: 'abc456',
+            quizzes_id: 'abc123',
+            user_id: 'user1',
+            UserAnswers: [2,2],
+            quizId = quizzes_id
+        });
+        var postRes = await postAnswers.save();
         console.log(getRes);
+        res.status(201).send('Added Answers')
+
+        // Fuction fires after answers have been posted
+        console.log(quizId);
+        getQuizAnswers()
+
     }
-    catch(err){
+    catch (err) {
         console.log('Error:' + '' + err);
     }
 }
 
-function getQuizAnswers(){
-    var score = 0;
-    var percentCorrect;
-    // TODO : Need to be updated to an array provide by user. A set of sample answers are hard coded below.
-    var quizAnswers = [2,2]
-    const quiz = await Quizzes.find({}).questions;
-
-    for (i = 0; i < quiz.questions.length[i]; i++){
-        if (quiz.questions[i] == quizAnswers[i]){
-            score ++;
-        }
+async function getResults(){
+    try {
+        var query  = Quizzes.where({ _id: req.body.quizId });
+        await query.findOne(function (err, quiz) {
+        if (err) return handleError(err)
+        if (quiz) {
+        var answers  = Answers.where({ quizzes_id: req.body.quizId });
+        await answers.findOne(function (err, answer){
+            if (err) return handleError(err);
+            if (answer) {
+                answer['quizResults'] = quiz
+                return res.status(201).send(answer) 
+            } 
+        })
     }
-    percentCorrect = (score / quizAnswers.length) * 100;
-    console.log(score, percentCorrect);
-    return percentCorrect;
+});
+    } catch (err) {
+        console.log('Error:' + '' + err);
+    }
 }
+
 
 module.exports.getQuiz = getQuiz;
 module.exports.postQuiz = postQuiz;
 module.exports.getQuizAnswers = getQuizAnswers;
+module.exports.postAnswers = postAnswers;
+module.exports.getResults = getResults;
+/* Model for the anwsers schema
+const answerSchema = new mongoose.Schema({
+    _id: {
+        type: String
+    },
+    quizzes_id: {
+        type: String
+    },
+    user_id: {
+        type: String
+    },
+    answers: {
+        type: Array
+    }
+});
+
+var query  = Quizzes.where({ _id: req.body.quizId });
+query.findOne(function (err, quiz) {
+  if (err) return handleError(err);
+  if (quiz) {
+    // doc may be null if no document matched
+  }
+});
+*/

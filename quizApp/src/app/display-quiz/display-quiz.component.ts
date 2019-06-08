329c67970fbad4b34c91870c0ad0d5d9b43096c8
AmySand <amy.sand@midlandu.edu>
2019-06-08 09:47:55 -0500

    info pulling from hard coded ts json

--- quizApp/src/app/display-quiz/display-quiz.component.ts
+++ quizApp/src/app/display-quiz/display-quiz.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { GetQuestionsService } from 'src/app/get-questions.service';
 
+
+
+export interface Quiz {
+title: string;
+description: string;
+questions: [
+  {prompt: string,
+  choices: [string, string, string, string],
+  answer: string},
+  {prompt: string,
+    choices: [string, string, string, string],
+    answer: string} ];
+}
+
+
 @Component({
   selector: 'app-display-quiz',
   templateUrl: './display-quiz.component.html',
@@ -10,10 +25,37 @@ export class DisplayQuizComponent implements OnInit {
 
   constructor(private questionsService: GetQuestionsService) { }
 
-  ngOnInit() {
-  }
+  //hard coded json for testing
+quiz: Quiz = {
+    title: 'HTML Quiz',
+    description: 'This is a quiz here',
+    questions: [
+      {
+      prompt: 'What does HTML Stand for?',
+      choices: ['yes this is a choice', 'no this is not a choice', 'maybe this a choice', 'unknown choice'],
+      answer: 'yes'
+    },
+    {
+      prompt: '2 What does HTML Stand for?',
+      choices: ['2 yes', '2 no', '2 maybe', '2 unknown'],
+      answer: '2 yes'
+    }]
+    };
 
-  displayQuestions() {
+    //isolating the questions
+    quizQuestions: Quiz['questions'] = this.quiz.questions;
+//insolating the page question
+currentChoices = this.quiz.questions[0].choices;
+
+
+    ngOnInit() {
+
+ this.quizQuestions.forEach(res => {
+      console.log(res);
+      
+});
+ }
+displayQuestions() {
     this.questionsService.getQuestion().subscribe(res => {
       let results = res;
     })
--- quizApp/src/app/quiz-service.service.ts
+++ quizApp/src/app/quiz-service.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class QuizServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+// getQuiz(){
+//   this.http.get();
 }
+

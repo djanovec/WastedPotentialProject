d14c825becdb8e5c4395349881ae7157367f4b36
AmySand <amy.sand@midlandu.edu>
2019-06-09 13:59:06 -0500

    successfully displaying next question

--- quizApp/src/app/app.module.ts
+++ quizApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -81,7 +81,8 @@ import { FormsModule } from '@angular/forms';
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
 
   ],
   providers: [],
--- quizApp/src/app/display-quiz/display-quiz.component.ts
+++ quizApp/src/app/display-quiz/display-quiz.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { GetQuestionsService } from 'src/app/get-questions.service';
-
+import {FormControl} from '@angular/forms';
 
 
 export interface Quiz {
@@ -10,6 +10,9 @@ questions: [
   {prompt: string,
   choices: [string, string, string, string],
   answer: string},
+  {prompt: string,
+    choices: [string, string, string, string],
+    answer: string},
   {prompt: string,
     choices: [string, string, string, string],
     answer: string} ];
@@ -22,7 +25,8 @@ questions: [
   styleUrls: ['./display-quiz.component.scss']
 })
 export class DisplayQuizComponent implements OnInit {
-
+  name = new FormControl('')
+  x = 0;
   constructor(private questionsService: GetQuestionsService) { }
 
   //hard coded json for testing
@@ -39,25 +43,41 @@ quiz: Quiz = {
       prompt: '2 What does HTML Stand for?',
       choices: ['2 yes', '2 no', '2 maybe', '2 unknown'],
       answer: '2 yes'
-    }]
+    },
+    {
+      prompt: '3 What does HTML Stand for?',
+      choices: ['3 yes', '3 no', '3 maybe', '3 unknown'],
+      answer: '3 yes'
+    }
+  ]
     };
 
-    //isolating the questions
-    quizQuestions: Quiz['questions'] = this.quiz.questions;
-//insolating the page question
-currentChoices = this.quiz.questions[0].choices;
+
+// isolating the page question
+
+currentQuestion = this.quiz.questions[this.x].prompt;
 
 
+// insolating the page choices
+currentChoices = this.quiz.questions[this.x].choices;
+
+// observing the changing x value
+// scope.$watch('x', function(newValue, oldValue) {
+//   console.log(newValue);
+//   scope.someVar = [Do something with someVar];
+// });
+
+// const questionObservable = (this.x);
+// const questionObserverObject = { next: x => console.log('Observer got a next value: ' + x)};
+// questionObservable.subscribe(currentChoices);
     ngOnInit() {
-
- this.quizQuestions.forEach(res => {
-      console.log(res);
-      
-});
  }
-displayQuestions() {
-    this.questionsService.getQuestion().subscribe(res => {
-      let results = res;
-    })
-  }
+
+ // function that activates on click of "next button." Changes the question.
+nextQuestion() {
+      this.x = this.x + 1;
+      this.currentQuestion = this.quiz.questions[this.x].prompt;
+      this.currentChoices = this.quiz.questions[this.x].choices;
+}
+
 }

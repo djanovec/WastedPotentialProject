f52c3434dba5c0a35061121ab9dd6b30f8d75b24
Dave J <davejanovec@gmail.com>
2019-06-08 21:36:54 -0500

    registration routing commented out

--- quizApp/src/app/app-routing.module.ts
+++ quizApp/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  // { path: 'registration', component: RegistrationComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 

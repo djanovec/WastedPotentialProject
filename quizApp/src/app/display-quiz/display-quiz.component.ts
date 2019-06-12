cfcaa8df179dd6124cce4a2f127a27c75acd21eb
AmySand <amy.sand@midlandu.edu>
2019-06-11 22:26:57 -0500

    Additional functionaly.
    Next button and correct answers added. Answers hidding until the submit button is clicked. Having an issue hidding the previous button and not affecting the othe functionality.

--- quizApp/src/app/display-quiz/display-quiz.component.ts
+++ quizApp/src/app/display-quiz/display-quiz.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { GetQuestionsService } from 'src/app/get-questions.service';
 import { FormControl, FormGroup, ControlValueAccessor } from '@angular/forms';
-import { MatRadioChange, MatButton} from '@angular/material';
+import { MatRadioChange, MatButton } from '@angular/material';
 
 // Object Interface for data
 export interface Quiz {
@@ -11,17 +11,17 @@ export interface Quiz {
     {
       prompt: string,
       choices: [string, string, string, string],
-      answer: string
+      correct: string
     },
     {
       prompt: string,
       choices: [string, string, string, string],
-      answer: string
+      correct: string
     },
     {
       prompt: string,
       choices: [string, string, string, string],
-      answer: string
+      correct: string
     }];
 }
 
@@ -33,6 +33,7 @@ export interface Quiz {
 
 
 export class DisplayQuizComponent implements OnInit {
+
   formControl = new FormControl('');
   x = 0;
   matButton: MatButton;
@@ -49,17 +50,17 @@ export class DisplayQuizComponent implements OnInit {
       {
         prompt: 'What does HTML Stand for?',
         choices: ['Hyper Text Markup Language', 'Hyperlinks and Text Markup Language', 'Home Tool Markup Language', 'Make Text Made Language'],
-        answer: 'Hyper Text Markup Language'
+        correct: 'Hyper Text Markup Language'
       },
       {
         prompt: 'Which character is used to indicate an end tag?',
         choices: ['/', '^', '*', '<'],
-        answer: '/'
+        correct: '/'
       },
       {
         prompt: 'How can you make a numbered list?',
         choices: ['<ul>', '<list>', '<nl>', '<ol>'],
-        answer: '<ol>'
+        correct: '<ol>'
       }
     ]
   };
@@ -72,64 +73,69 @@ export class DisplayQuizComponent implements OnInit {
   // insolating the page choices
   currentChoices = this.quiz.questions[this.x].choices;
 
-  ngOnInit() {
-  }
+  correctAnswer = this.quiz.questions[this.x].correct;
+
+  ngOnInit() { }
+
   // identifying which radio button is selected
   onSelectionChange(currentChoice) {
     this.selectedRadio = currentChoice;
     console.log(this.selectedRadio);
-
   }
 
   unhideSubmitButton() {
-     document.getElementById('searchsubmit').id = 'visible';
-
-  }
-
-  unhidePreviousButton() {
     document.getElementById('searchsubmit').id = 'visible';
 
- }
-
-  submitButton() {
-  const questionArea = document.getElementsByClassName('questionContainer')[0];
-  questionArea.remove();
-  document.getElementById('thankYou').id = 'visible';
   }
+
+  // unhidePreviousButton() {
+  //   document.getElementById('previousB').id = 'visible2';
+  //   var executed = false;
+  //   return function () {
+  //     if (!executed) {
+  //       executed = true;
+  //     }
+  //   };
+
+  // }
+
+
+
+
 
   // function that activates on click of "next button." Changes the question.
   nextQuestion() {
+    // this.unhidePreviousButton();
     this.userAnswers.push(this.selectedRadio);
     console.log(this.userAnswers);
     this.x = this.x + 1;
-    if ( this.x < this.quiz.questions.length){
-    this.currentQuestion = this.quiz.questions[this.x].prompt;
-    this.currentChoices = this.quiz.questions[this.x].choices;
-  }
+
+    if (this.x < this.quiz.questions.length) {
+      this.currentQuestion = this.quiz.questions[this.x].prompt;
+      this.currentChoices = this.quiz.questions[this.x].choices;
+      this.correctAnswer = this.quiz.questions[this.x].correct;
+    }
     else {
-  this.unhideSubmitButton();
+      this.unhideSubmitButton();
     }
   }
 
-  //previous button
-  // previousQuestion() {
-  //   if ( this.x = 1 ) {
-  //     this.unhidePreviousButton();
-  //   }
+  // previous button
+  previousQuestion() {
+    this.userAnswers.pop();
+    console.log(this.userAnswers);
+    this.x = this.userAnswers.length;
+    this.currentQuestion = this.quiz.questions[this.x].prompt;
+    this.currentChoices = this.quiz.questions[this.x].choices;
+    this.correctAnswer = this.quiz.questions[this.x].correct;
+  }
 
-  //   this.userAnswers.pop;
-  //   // console.log(this.userAnswers);
-  //   this.x = this.x - 1;
-  //   if ( this.x < this.quiz.questions.length) {
-  //   this.currentQuestion = this.quiz.questions[this.x].prompt;
-  //   this.currentChoices = this.quiz.questions[this.x].choices;
-  // }
-  // //   else {
-  // // this.unhideSubmitButton();
-  // //   }
-  // }
-
-
+  submitButton() {
+    const questionArea = document.getElementsByClassName('questionContainer')[0];
+    questionArea.remove();
+    document.getElementById('thankYou').id = 'visible';
+    for (let z = 0; z < this.userAnswers.length; z++) {
+      const correctAnswer = document.getElementById('hidden2').id = 'visible';
+    }
+  }
 }
-
-

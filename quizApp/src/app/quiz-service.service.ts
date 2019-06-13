ded70ed288ae8a6383bcda92784a4a7d7671cf3f
Ian Gurney <ianclaytongurney@gmail.com>
2019-06-13 09:41:05 -0500

    i did what amy did alaredy

--- quizApp/src/app/quiz-service.service.ts
+++ quizApp/src/app/quiz-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { Quiz } from './display-quiz/display-quiz.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,32 @@ export class QuizServiceService {
 
 // getQuiz(){
 //   this.http.get();\
+sampleQuiz={
+  title: 'HTML Quiz',
+  description: 'This is a quiz here',
+  questions: [
+    {
+      prompt: 'What does HTML Stand for?',
+      choices: ['Hyper Text Markup Language', 'Hyperlinks and Text Markup Language', 'Home Tool Markup Language', 'Make Text Made Language'],
+      correct: 'Hyper Text Markup Language'
+    },
+    {
+      prompt: 'Which character is used to indicate an end tag?',
+      choices: ['/', '^', '*', '<'],
+      correct: '/'
+    },
+    {
+      prompt: 'How can you make a numbered list?',
+      choices: ['<ul>', '<list>', '<nl>', '<ol>'],
+      correct: '<ol>'
+    }
+  ]
+};
 
 constructor(private http: HttpClient, private router: Router) {}
 
-  private savedQuiz = new BehaviorSubject('');
-  quiz = this.savedQuiz.asObservable();
+  private savedQuiz = new BehaviorSubject<Object>(this.sampleQuiz);
+  quiz: Quiz;
   errorMsg: string;
 
   getQuizByToken(token){
@@ -24,7 +46,8 @@ constructor(private http: HttpClient, private router: Router) {}
       return this.errorMsg="No quiz found";
       }
       else {
-      this.router.navigate(['/take_quiz'])
+        this.quiz = res as Quiz;
+        this.router.navigate(['/take_quiz'])
       return;
     }});
   }
